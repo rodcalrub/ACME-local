87aff58efe46dcdd8f8edadca322d4ea34d70b2a
rodcalrub <rodcalrub@alum.us.es>
2021-04-05 12:38:29 +0200

    Added nginx variables

--- start.sh
+++ start.sh
@@ -8,20 +8,25 @@ docker volume create --name=logsvol
 docker volume create --name=datavol
 
 #Development
-export NODE_ENV=development
-export PORT=8001
-export DBPORT=27017
-export DBSTRING="mongodb://mongo27017:27017/acme-explorer-${NODE_ENV}";
-export HOSTNAME=${NODE_ENV}.${BASE_SITE}
-source secrets.sh
-docker-compose -p ${VIRTUAL_HOST} up -d
-
-# #Production
 # export NODE_ENV=production
 # export PORT=8081
 # export DBPORT=27018
-# export HOSTNAME=${NODE_ENV}.${BASE_SITE}
-# export DBSTRING="mongodb://mongo27017:27017/acme-explorer-${NODE_ENV}";
-# docker-compose -p ${VIRTUAL_HOST} up -d
+# export HOSTNAME=development.${BASE_SITE}
+# export VIRTUAL_HOST=${HOSTNAME}
+# export VIRTUAL_PORT=${PORT}
+# export DBSTRING="mongodb://mongo${DBPORT}:${DBPORT}/acme-explorer-${NODE_ENV}";
+# source secrets.sh
+# docker-compose -p ${HOSTNAME} up -d
+
+# #Production
+export NODE_ENV=production
+export PORT=8081
+export DBPORT=27018
+export HOSTNAME=${NODE_ENV}.${BASE_SITE}
+export VIRTUAL_HOST=${HOSTNAME}
+export VIRTUAL_PORT=${PORT}
+export DBSTRING="mongodb://mongo${DBPORT}:${DBPORT}/acme-explorer-${NODE_ENV}";
+source secrets.sh
+docker-compose -p ${HOSTNAME} up -d
 
 # docker run --name busybox -v logsvol:/var/log/app -it  busybox sh
--- stop.sh
+++ stop.sh
@@ -3,22 +3,24 @@ BASE_SITE=acmeexplorer.com
 docker stop nginx-proxy
 
 # #Development
-export NODE_ENV=development
-export PORT=8001
-export DBPORT=27017
-export DBSTRING="mongodb://mongo27017:27017/acme-explorer-${NODE_ENV}";
-export HOSTNAME=${NODE_ENV}.${BASE_SITE}
-source secrets.sh
-docker-compose -p ${VIRTUAL_HOST} down
+# export NODE_ENV=development
+# export PORT=8001
+# export DBPORT=27017
+# export DBSTRING="mongodb://mongo27017:27017/acme-explorer-${NODE_ENV}";
+# export HOSTNAME=${NODE_ENV}.${BASE_SITE}
+# source secrets.sh
+# docker-compose -p ${HOSTNAME} down
 
 #Production
-# export NODE_ENV=production
-# export PORT=8081
-# export DBPORT=27018
-# export DBSTRING="mongodb://mongo27017:27017/acme-explorer-${NODE_ENV}";
-# export VIRTUAL_HOST=${NODE_ENV}.${BASE_SITE}
-# source secrets.sh
-# docker-compose -p ${VIRTUAL_HOST} down
+export NODE_ENV=production
+export PORT=8081
+export DBPORT=27018
+export VIRTUAL_HOST=${HOSTNAME}
+export VIRTUAL_PORT=${PORT}
+export DBSTRING="mongodb://mongo${DBPORT}:${DBPORT}/acme-explorer-${NODE_ENV}";
+export HOSTNAME=${NODE_ENV}.${BASE_SITE}
+source secrets.sh
+docker-compose -p ${HOSTNAME} down
 
 #Teardown
 docker stop nginx-proxy
